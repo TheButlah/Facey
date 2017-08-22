63e75fa62c7894aa12280c51559e93a666bfd961
Yuji Akimoto <ya242@cornell.edu>
2017-08-22 00:26:07 -0400

    Added image demo prototype

---
+++ pygame_test.py
@@ -0,0 +1,18 @@
+import pygame
+import pygame.camera
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    pygame.camera.init()
+    cam = pygame.camera.Camera("/dev/video0", (640, 480))
+    cam.start()
+    img = cam.get_image()
+    cam.stop()
+    raw = img.get_buffer().raw
+    arr = np.flip(np.frombuffer(raw, dtype=np.ubyte).reshape(480, 640, 3), axis=2)
+    plt.imshow(arr)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
